8bf3db271b455a4227b2630adb02503e32935f05
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-11-23 23:48:50 +0530

    template changes

--- mongo/models.py
+++ mongo/models.py
@@ -42,47 +42,28 @@ class User:
     
       
       
-      elif login_form.submit2.data and login_form.validate():
-        
+      elif login_form.submit2.data and login_form.validate(): 
         user = db.users.find_one({
-         "username" : login_form.username.data,
-        })
+         "username" : login_form.username.data })
 
         if user and sha256_crypt.verify(login_form.password.data, user['password']):
           self.start_session(user)
           return render_template('dashboard.html')
 
-      print(login_form.submit2.data, "x", login_form.validate())
-      error = 'Invalid login'
+
+      error = 'Check details!'
       return render_template('register.html', error=error,  reg_form=reg_form, login_form=login_form)
   
     else:
       return render_template('register.html', reg_form=reg_form, login_form=login_form)
 
-
-  
-
-      
-  
   def logout(self):
     
     session.clear()
     flash("You are now logged out", "success")
-    return redirect('/login')
+    return redirect('/register')
   
-  def login(self):
-    if request.method == 'POST':
-            
-      user = db.users.find_one({
-        "username" : request.form['username_l']
-      })
+     
 
-      if user and sha256_crypt.verify(request.form['password_l'], user['password']):
-        self.start_session(user)
-        return render_template('dashboard.html')
-
-      
-      
-
-    return render_template('login.html')
+  
    
