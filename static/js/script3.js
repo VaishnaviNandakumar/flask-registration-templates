a6e89f110fac6e7a69db6a9e71af5ff5492a3758
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-12-05 16:39:08 +0530

    Drop files from .gitignore

--- static/js/script3.js
+++
@@ -1,9 +0,0 @@
-const regButton = document.getElementById('reg');
-const signIn = document.getElementById('signin');
-const signUp = document.getElementById('signup');
-
-regButton.addEventListener('click', () => {
-    signIn.style.display = "none";
-    signUp.style.display = "flex";
-    
-});
